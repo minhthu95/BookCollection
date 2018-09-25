dde50fd1e9c47f1b4cbfe13d4964feecdbfaa0e8
minhthu95 <thu.ho@student.laurea.fi>
2018-09-25 12:29:52 +0300

    Bootstrap added (navigation bar, pictures)

--- app_server/controllers/ctrlFiction.js
+++ app_server/controllers/ctrlFiction.js
@@ -2,13 +2,13 @@ const fictionlist = function(req, res){
     res.render('fiction',{
         fictions:
             [
-                {author:'Francis Scott Fitzgerald', name:'The Great Gatsby'},
-                {author:'William Shakespeare', name:'Romeo And Juliet'},
-                {author:'Graf Leo Tolstoy', name:'Anna Karenina'},
-                {author:'Mark Twain', name:'Adventures of Huckleberry Finn'},
-                {author:'Charles Dickens', name:'A Tale of Two Cities'},
-                {author:'William Shakespeare', name:'Hamlet'},
-                {author:'Alexandre Dumas', name:'The Count of Monte Cristo'}
+                {image:'images/fiction/the_great_gatsby.jpg',author:'Francis Scott Fitzgerald', name:'The Great Gatsby'},
+                {image:'images/fiction/romeo_and_juliet.jpg',author:'William Shakespeare', name:'Romeo And Juliet'},
+                {image:'images/fiction/anna_karenina.jpg',author:'Graf Leo Tolstoy', name:'Anna Karenina'},
+                {image:'images/fiction/huckleberry_finn.jpg',author:'Mark Twain', name:'Adventures of Huckleberry Finn'},
+                {image:'images/fiction/two_cities.jpg',author:'Charles Dickens', name:'A Tale of Two Cities'},
+                {image:'images/fiction/hamlet.jpg', author:'William Shakespeare', name:'Hamlet'},
+                {image:'images/fiction/monte_cristo.jpg', author:'Alexandre Dumas', name:'The Count of Monte Cristo'}
             ]});
 };
 module.exports = {
--- app_server/controllers/ctrlNovel.js
+++ app_server/controllers/ctrlNovel.js
@@ -2,13 +2,13 @@ const novellist = function(req, res){
     res.render('novel',{
         novels:
             [
-                {author:'Jack London', name:'The Call Of The Wild'},
-                {author:'Victor Hugo', name:'Les Miserables'},
-                {author:'Arthur Conan Doyle', name:'The Adventures of Sherlock Holmes'},
-                {author:'Bram Stoker', name:'Dracula'},
-                {author:'Robert Louis Stevenson', name:'Treasure Island'},
-                {author:'Jack London', name:'The Little Lady of the Big House'},
-                {author:'Lyman Frank Baum', name:'The Wonderful Wizard of Oz'}
+                {image:'images/novel/call_of_the_wild.jpg', author:'Jack London', name:'The Call Of The Wild'},
+                {image:'images/novel/les_miserables.jpg', author:'Victor Hugo', name:'Les Miserables'},
+                {image:'images/novel/sherlock_holmes.jpg', author:'Arthur Conan Doyle', name:'The Adventures of Sherlock Holmes'},
+                {image:'images/novel/dracula.jpg', author:'Bram Stoker', name:'Dracula'},
+                {image:'images/novel/treasure_island.jpg', author:'Robert Louis Stevenson', name:'Treasure Island'},
+                {image:'images/novel/little_lady.jpg', author:'Jack London', name:'The Little Lady of the Big House'},
+                {image:'images/novel/wizard_of_oz.jpg', author:'Lyman Frank Baum', name:'The Wonderful Wizard of Oz'}
             ]});
 };
 module.exports = {
