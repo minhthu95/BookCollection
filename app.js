2369511ac8130498dabcfab0116d47d86f016dc6
minhthu95 <thu.ho@student.laurea.fi>
2018-10-03 16:40:52 +0300

    added appSchemas.js and db.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+require('./app_server/models/db');
 
 const index = require('./app_server/routes/index');
 const users = require('./app_server/routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
